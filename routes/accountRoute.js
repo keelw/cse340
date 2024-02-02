339f84b50ba936af7d0a52f8169fe3c80793b9c9
keelw <wjordankeel@gmail.com>
2024-02-02 16:59:18 -0700

    w04 learning activities complete

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation")
 
 // Route to build account login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -10,8 +11,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build the registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-// Route to submit the registration form
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
-
+// Route to submit the registration form and validate
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 // Exports
 module.exports = router;
